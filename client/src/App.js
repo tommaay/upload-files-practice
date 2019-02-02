0db3832d410ac7d518bfc745edf96aa4e4cc3e12
ToMMaaY <thuynh629@gmail.com>
2019-02-02 09:57:21 -0800

    updated the form to take in more inputs

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,26 @@ class App extends Component {
             <h1>Upload Image Project</h1>
             <form>
                <div className="form-group">
+                  <label htmlFor="title">Title</label>
+                  <input type="text" id="title" name="title" required />
+
+                  <label htmlFor="price">Price</label>
+                  <input type="number" id="price" name="price" required />
+
+                  <label htmlFor="label">Label</label>
+                  <input type="text" id="label" name="label" required />
+
                   <label htmlFor="image">Upload image: </label>
-                  <input type="file" id="image" name="image" accept="image/*" />
+                  <input
+                     type="file"
+                     id="image"
+                     name="image"
+                     accept="image/*"
+                     required
+                  />
                </div>
+
+               <button>Upload</button>
             </form>
          </div>
       );
