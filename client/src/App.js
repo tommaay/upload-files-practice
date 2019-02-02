37e7933e47fefcda30752769654c7b9430bf31a9
ToMMaaY <thuynh629@gmail.com>
2019-02-02 09:38:57 -0800

    created the upload file form in React

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Upload Image Project</h1>
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div className="App">
+            <h1>Upload Image Project</h1>
+            <form>
+               <div className="form-group">
+                  <label htmlFor="image">Upload image: </label>
+                  <input type="file" id="image" name="image" accept="image/*" />
+               </div>
+            </form>
+         </div>
+      );
+   }
 }
 
 export default App;
