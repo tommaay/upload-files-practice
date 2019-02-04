83f25ac3002b433b67c0cf3e479bcf35b0a2d6a3
ToMMaaY <thuynh629@gmail.com>
2019-02-04 14:47:07 -0800

    created a cloudinaryConfig middleware

--- api/server.js
+++ api/server.js
@@ -2,14 +2,6 @@ const express = require('express');
 const server = express();
 const middleware = require('../config/middleware');
 
-var multer = require('multer');
-
-var storage = multer.diskStorage({
-   filename: (req, file, callback) => {
-      callback(null, Date.now() + file.originalname);
-   },
-});
-
 var imageFilter = (req, file, cb) => {
    // accept image files only
    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
@@ -17,15 +9,6 @@ var imageFilter = (req, file, cb) => {
    }
    cb(null, true);
 };
-
-var upload = multer({ storage: storage, fileFilter: imageFilter });
-
-var cloudinary = require('cloudinary');
-cloudinary.config({
-   cloud_name: 'tommaay',
-   api_key: process.env.CLOUDINARY_API_KEY,
-   api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 middleware(server);
 
