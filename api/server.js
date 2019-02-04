7862ba9d795db04d726aa06797d1a2fa25d6863c
ToMMaaY <thuynh629@gmail.com>
2019-02-04 12:44:18 -0800

    created a form to upload files for the client

--- api/server.js
+++ api/server.js
@@ -2,6 +2,31 @@ const express = require('express');
 const server = express();
 const middleware = require('../config/middleware');
 
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+   filename: (req, file, callback) => {
+      callback(null, Date.now() + file.originalname);
+   },
+});
+
+var imageFilter = (req, file, cb) => {
+   // accept image files only
+   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+      return cb(new Error('Only image files are allowed!'), false);
+   }
+   cb(null, true);
+};
+
+var upload = multer({ storage: storage, fileFilter: imageFilter });
+
+var cloudinary = require('cloudinary');
+cloudinary.config({
+   cloud_name: 'tommaay',
+   api_key: process.env.CLOUDINARY_API_KEY,
+   api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 middleware(server);
 
 server.get('/', (req, res) => {
